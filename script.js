11bce63af211031b70cec83d5515a044175ba0f2
gtsk415 <print.bistro@mail.ru>
2024-04-04 16:22:32 +0300

    add html,css,js

---
+++ script.js
@@ -0,0 +1,11 @@
+const slider = document.querySelector('.slider');
+const before = slider.querySelector('.before');
+const beforeImage = before.querySelector('img');
+const change = slider.querySelector('.change');
+const body = document.body;
+
+let isActive =false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+})
