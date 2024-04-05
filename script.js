908b8cd2c5c3fa858bd3f1f2db09f2dcfd1a2816
gtsk415 <print.bistro@mail.ru>
2024-04-05 14:29:41 +0300

    work

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ const beforeImage = before.querySelector('img');
 const change = slider.querySelector('.change');
 const body = document.body;
 
-let isActive =false;
+let isActive = false;
 
 document.addEventListener('DOMContentLoaded', () => {
-  
-})
+  let width = slider.offsetWidth;
+  beforeImage.style.width = `${width}px`
+});
+
+const beforeAfterSlider = (x) => {
+   let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+   before.style.width = `${shift}px`;
+   change.style.left = `${shift}px`;
+};
+
+beforeAfterSlider.addEventListener('mousemove', (e) => {
+  if (!isActive) {
+    return;
+  }
+});
